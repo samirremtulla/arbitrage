4507b03131562c2b32bb2a8e5d54cd28a44ade48
Samir Remtulla <samir.remtulla@gmail.com>
2013-04-10 18:23:54 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arbitrage::Application.config.secret_token = '035c8a5140c7a59ce9abc0abab7ef1cb53e381e63c12be41a0fcd05b57a7ff2747e685fbfb04c9ca027a891b2152800cf9873e8169a38cee3931cd1b6e00e822'
